608059e33f9e815c7588e12553f02c8e572b3e37
Robert <35431956+robertranjan@users.noreply.github.com>
2018-12-06 14:59:42 -0800

    update sha256

--- gettty.rb
+++ gettty.rb
@@ -3,7 +3,7 @@ class Gettty < Formula
   homepage "https://github.com/devops-engineer"
   url "https://github.com/devops-engineer/homebrew-gettty/archive/v0.1.2-alpha.tar.gz"
   version "0.1.2-alpha"
-  sha256 "b94181ab99a1f6fec94dc45b537490209c9341a5c257d5ae23ee8843be3d0af7"
+  sha256 "294efe0d068e9130975dfae882f79e25be7b0b21acbbf9f26577a6ba3579aada"
   # depends_on "cmake" => :build
 
 	bottle :unneeded
