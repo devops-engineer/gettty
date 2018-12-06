228a526d504351163f85d4d1722eadb9c00ab47f
Robert <35431956+robertranjan@users.noreply.github.com>
2018-12-06 14:57:24 -0800

    updated archive link

--- gettty.rb
+++ gettty.rb
@@ -1,7 +1,7 @@
 class Gettty < Formula
   desc "Get a tty of a kubernetes container"
   homepage "https://github.com/devops-engineer"
-  url "https://github.com/devops-engineer/homebrew-gettty/archive/v0.1.1-alpha.tar.gz"
+  url "https://github.com/devops-engineer/homebrew-gettty/archive/v0.1.2-alpha.tar.gz"
   version "0.1.2-alpha"
   sha256 "b94181ab99a1f6fec94dc45b537490209c9341a5c257d5ae23ee8843be3d0af7"
   # depends_on "cmake" => :build
