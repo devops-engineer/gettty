3adb3f36b8815eb1f481e5e3d30977896f2a7a72
Robert <35431956+robertranjan@users.noreply.github.com>
2018-12-06 16:06:19 -0800

    new release

--- gettty.rb
+++ gettty.rb
@@ -1,9 +1,9 @@
 class Gettty < Formula
   desc "Get a tty of a kubernetes container"
   homepage "https://github.com/devops-engineer"
-  url "https://github.com/devops-engineer/homebrew-gettty/archive/v0.1.2-alpha.tar.gz"
-  version "0.1.2-alpha"
-  sha256 "294efe0d068e9130975dfae882f79e25be7b0b21acbbf9f26577a6ba3579aada"
+  url "https://github.com/devops-engineer/homebrew-gettty/archive/v0.1.3-alpha.tar.gz"
+  version "0.1.3-alpha"
+  sha256 "d0b8fd5a7819fb1278d3e1764ba69d13d585f414416c460615bcf51329db6152"
   # depends_on "cmake" => :build
 
 	bottle :unneeded
