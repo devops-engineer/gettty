bbb43c8be2ad3a39dc5578307385643f245474c3
Robert Thanulingam <rranjan3@apple.com>
2018-12-18 13:27:19 -0800

    increment version & updated install instruction.

--- gettty.rb
+++ gettty.rb
@@ -1,14 +1,14 @@
 class Gettty < Formula
   desc "Get a tty of a kubernetes container"
   homepage "https://github.com/devops-engineer"
-  url "https://github.com/devops-engineer/homebrew-gettty/archive/v0.1.3-alpha.tar.gz"
-  version "0.1.3-alpha"
+  url "https://github.com/devops-engineer/homebrew-gettty/archive/v0.5-alpha.tar.gz"
+  version "0.5"
   sha256 "d0b8fd5a7819fb1278d3e1764ba69d13d585f414416c460615bcf51329db6152"
-  # depends_on "cmake" => :build
 
 	bottle :unneeded
 
   def install
-	bin.install "gettty"
+    system "echo", "source /usr/local/Homebrew/Library/Taps/rranjan3/homebrew-tools/gettty/gettty", ">>", "~/.bash_profile"
+    bin.install "install-gettty.sh"
   end
 end
