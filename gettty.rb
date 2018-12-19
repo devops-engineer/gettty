b0310263014f8ab4740934d20109aaa8504d2b3c
Robert Ranjan Thanulingam <robertranjan@gmail.conm>
2018-12-18 22:39:50 -0800

    updated link and shasum

--- gettty.rb
+++ gettty.rb
@@ -1,11 +1,11 @@
 class Gettty < Formula
   desc "Get a tty of a kubernetes container"
   homepage "https://github.com/devops-engineer"
-  url "https://github.com/devops-engineer/homebrew-gettty/blob/master/archive/gettty-0.5.tar.gz"
+  url "https://github.com/devops-engineer/homebrew-gettty/archive/v0.5.tar.gz"
   version "0.5"
-  sha256 "d0b8fd5a7819fb1278d3e1764ba69d13d585f414416c460615bcf51329db6152"
+  sha256 "f13edc26ebb7a88f6255cf77d36f89937a8465616ae0eebb275c31f22d7feb78"
 
-	bottle :unneeded
+  bottle :unneeded
 
   def install
     system "echo", "source /usr/local/Homebrew/Library/Taps/rranjan3/homebrew-tools/gettty/gettty", ">>", "~/.bash_profile"
