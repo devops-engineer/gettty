b4f161c4f9502123984eb959688233da6919c593
Robert Ranjan Thanulingam <robertranjan@gmail.conm>
2018-12-18 22:29:53 -0800

    url updated

--- gettty.rb
+++ gettty.rb
@@ -1,7 +1,7 @@
 class Gettty < Formula
   desc "Get a tty of a kubernetes container"
   homepage "https://github.com/devops-engineer"
-  url "https://github.com/devops-engineer/homebrew-gettty/archive/v0.5.tar.gz"
+  url "https://github.com/devops-engineer/homebrew-gettty/blob/master/archive/gettty-0.5.tar.gz"
   version "0.5"
   sha256 "d0b8fd5a7819fb1278d3e1764ba69d13d585f414416c460615bcf51329db6152"
 
